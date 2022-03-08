de852eb53b0b240dc97e6e98b8985192984ffd4b
Hairul Lana <hairullana99@gmail.com>
2022-03-08 13:43:20 +0800

    change asset to cdn

--- headtags.php
+++ headtags.php
@@ -1,14 +1,12 @@
-<link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
-<link href="assets/fontawesome/css/all.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- Sweetalert2 -->
-<link rel="stylesheet" href="assets/sweetalert2/sweetalert2.min.css">
-<!-- Sweetalert2 JS -->
-<script src="assets/sweetalert2/sweetalert2.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <!-- Favicon -->
 <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
 <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
 
 <!-- datatables -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css" />
-<script src="assets/jquery/jquery-3.5.1.js"></script>
-<script src="assets/jquery/jquery.dataTables.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
